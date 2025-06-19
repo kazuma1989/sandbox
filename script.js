dcd9e2e9f3e6bee0059c120bd65ec4ca431aaeb9
kazuma1989 <kazuma.ebina@quipper.com>
2025-06-20 05:59:51 +0900

    with built-in server

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import { spawn } from "node:child_process";
+import { createServer } from "node:http";
 import { setTimeout } from "node:timers/promises";
 
 using _servor = spawn("npm", ["run", "servor", "--port=8001"], {
@@ -9,6 +10,14 @@ using _sirv = spawn("npm", ["run", "sirv", "--port=9001", "--", "--host", "172.1
   stdio: "inherit",
 });
 
+await using server = createServer((req, res) => {
+  res.end("Hello World");
+});
+
+server.listen(7001, () => {
+  console.log("Server is running on port 8000");
+});
+
 await setTimeout(5_000);
 
 const result = await Promise.allSettled([
@@ -17,6 +26,9 @@ const result = await Promise.allSettled([
 
   fetch("http://localhost:9001/"),
   fetch("http://172.18.0.2:9001/"),
+
+  fetch("http://localhost:7001/"),
+  fetch("http://172.18.0.2:7001/"),
 ]);
 
 console.log(...result);
